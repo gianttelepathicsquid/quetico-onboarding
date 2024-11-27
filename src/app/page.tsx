2d1c4a7e5b2cc5a5c6a02c7815014aea7c073342
gianttelepathicsquid <kevinxrp@gmail.com>
2024-11-26 22:33:14 -0800

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { QueticoOnboardingApp } from '@/components/checklist'
+
+export default function Home() {
+  return (
+    <div className="container mx-auto py-8">
+      <QueticoOnboardingApp />
+    </div>
+  )
+}
