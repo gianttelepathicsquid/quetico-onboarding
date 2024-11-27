ba6eaed1587f187fdd11e3db7fb5c5bf0b0a4bd1
gianttelepathicsquid <kevinxrp@gmail.com>
2024-11-27 10:06:01 -0800

    Create sections.ts

---
+++ src/data/sections.ts
@@ -0,0 +1,19 @@
+import { Section } from '@/lib/types';
+
+export const sections: Section[] = [
+  {
+    title: "1. CUSTOMER INFORMATION AND SETUP",
+    items: [
+      {
+        title: "Gather Customer Information:",
+        items: [
+          "Customer Name",
+          "Key Contacts",
+          "Preferred Communication Channels",
+          "Account Numbers"
+        ]
+      },
+      // ... rest of your sections data as previously provided
+    ]
+  }
+];
